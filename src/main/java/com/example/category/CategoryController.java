0965247c552155bce996bf267dc64aa08ae9c15b
Adrian Rubak <adek.rubak@gmail.com>
2021-12-24 14:14:28 +0100

    simmilar name of api request

--- src/main/java/com/example/category/CategoryController.java
+++ src/main/java/com/example/category/CategoryController.java
@@ -3,12 +3,14 @@ package com.example.category;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@RestController("/categories")
+@RestController
+@RequestMapping("/api/category")
 public class CategoryController {
 
     CategoryRepository categoryRepository;
@@ -16,8 +18,9 @@ public class CategoryController {
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+
     @CrossOrigin(origins = "*")
-    @GetMapping("/all")
+    @GetMapping
     public List<Category> getCategories() {
         System.out.println("cos");
         return categoryRepository.getCategories();
