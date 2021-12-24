cb780935f4dd7c7fba682c4f908eb30c9a5363e0
Adrian Rubak <adek.rubak@gmail.com>
2021-12-24 13:54:07 +0100

    adding cors annotation

--- src/main/java/com/example/advert/AdvertisementController.java
+++ src/main/java/com/example/advert/AdvertisementController.java
@@ -1,6 +1,7 @@
 package com.example.advert;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class AdvertisementController {
         this.advertisementService = advertisementService;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping
     public List<Advertisement> getAll(){
         return advertisementService.findAll();
