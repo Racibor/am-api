aa33b8c9bd1cda7d1f457893e019b43e7127a304
Raberr <mustang19@onet.eu>
2022-01-23 20:05:42 +0100

    added login

--- src/main/java/com/example/advert/AdvertisementController.java
+++ src/main/java/com/example/advert/AdvertisementController.java
@@ -47,6 +47,7 @@ public class AdvertisementController {
     @PostMapping("")
     public String add(@RequestBody Advertisement advertisement){
         Advertisement save = advertisementService.save(advertisement);
+        System.out.println(advertisement.getUser());
         System.out.println(advertisement.getBase64Image().toString().substring(0, 100));
         System.out.println("adding");
         User user = (User)httpSession.getAttribute("user");
